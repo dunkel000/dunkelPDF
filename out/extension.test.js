99fef9b262cebbec72bf7f97ea944886aaebe801
Diego-Ignacio Ortiz <31400790+dunkel000@users.noreply.github.com>
2025-09-30 15:03:50 -0300

    added debug for vs code and working

---
+++ out/extension.test.js
@@ -0,0 +1,8 @@
+"use strict";
+const assert = require('assert');
+describe('Extension Tests', () => {
+    it('hello world!', () => {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
+//# sourceMappingURL=extension.test.js.map
